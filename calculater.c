560d46e5999718f4805b7a87f2aa8eca98bf9659
mohammedsameer1901 <sameermohammed1901@gmail.com>
2025-04-11 18:58:57 +0530

    SWITCH CASE IMPLEMENTED

--- calculater.c
+++ calculater.c
@@ -23,18 +23,42 @@ int ssqrt(int a){
 
 int main(){
 
-    int a,b;//3
+    int a,b;
     printf("Welcome to the Team Calculator!\n");
     printf("Enter first number: ");
     scanf("%d", &a);
     printf("Enter second number: ");
     scanf("%d", &b);
+    printf("Addition=a\n");
+    printf("Substraction=s\n");
+    printf("Multiplication=m\n");
+    printf("division=d\n");
+    printf("power=p\n");
+    printf("square root=r\n");
+    printf("Enter your choice as given charecter\n");
+   
+    char c;
+    scanf(" %c",&c);
+    switch(c){
+        case 'a':
+        printf("Addition of two numbers is: %d\n" ,add(a,b));
+        break;
+        case 's':
+        printf("Subtraction of two numbers is: %d\n" ,sub(a,b));
+        break;
+        case 'm':
+        printf("Mulitpilcation of two numbers is: %d\n" ,mult(a,b));
+        break;
+        case 'd': 
+        printf("Division of two numbers is: %d\n" ,div(a,b));
+        break;
+        case 'r':
+        printf("square root of %d = %d\n",a,ssqrt(a));
+        break;
+        case 'p':
+        printf("%d power %d  = %d\n",a,b,pow(a,b));
+        break;
 
-    printf("Addition of two numbers is: %d\n" ,add(a,b));
-    printf("Subtraction of two numbers is: %d\n" ,sub(a,b));
-    printf("Mulitpilcation of two numbers is: %d\n" ,mult(a,b));
-    printf("Division of two numbers is: %d\n" ,div(a,b));
-    printf("%d power %d = %d\n",a,b,power(a,b));//power
-    printf("square root of %d = %d\n",a,ssqrt(a));
+    }
     printf("\nCompleted 7 th Assignment");
 }
